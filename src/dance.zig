92c1c7afc3857c64f72ca6003adfa324305f404f
SuperAuguste <auguste.rame@gmail.com>
2021-07-13 22:53:03 -0400

    Comments, fixes

--- src/dance.zig
+++ src/dance.zig
@@ -19,20 +19,32 @@ test {
     _ = getPreferredBitSize();
 }
 
+/// Uses vector blocks to quickly determine equality
+/// Runs 20x faster on my machine; larger blocks are better (use getPreferredBitSize!)
 pub fn eql(comptime T: type, comptime block_size: usize, a: []const T, b: []const T) bool {
     if (a.len != b.len) return false;
 
     const Vector = std.meta.Vector(block_size, T);
     var index: usize = 0;
 
-    while (index < a.len - block_size) : (index += block_size) {
-        var av: Vector = a[index..][0..block_size].*;
-        var bv: Vector = b[index..][0..block_size].*;
+    if (a.len > block_size) {
+        while (index < a.len - block_size) : (index += block_size) {
+            var av: Vector = a[index..][0..block_size].*;
+            var bv: Vector = b[index..][0..block_size].*;
 
-        if (!@reduce(.And, av == bv)) return false;
-    }
+            if (!@reduce(.And, av == bv)) return false;
+        }
+    } else index += block_size;
 
     return std.mem.eql(u8, a[index - block_size ..], b[index - block_size ..]);
+}
+
+test "eql" {
+    try std.testing.expect(eql(u8, 8, "abc", "abc"));
+    try std.testing.expect(!eql(u8, 8, "abc", "abcd"));
+
+    try std.testing.expect(eql(u8, 8, "abcdfty78uhgfdxsedrtyghvfxdzesr80hBRUHugyt799t8oguvhckdi9rtgvc", "abcdfty78uhgfdxsedrtyghvfxdzesr80hBRUHugyt799t8oguvhckdi9rtgvc"));
+    try std.testing.expect(!eql(u8, 8, "abcdfty78uhgfdxsedrtyghvfxdzesr80hBRUHugyt799t8oguvhckdi9rtgvc", "abcdfty78uhgfdxsedrtyghvfxdzesr80hBRUHugyt799t8oguvhckdi9rtgvca"));
 }
 
 /// Uses a mask to find a char in a string; 11-20x as fast as std.mem.indexOfScalar on long strings
@@ -43,21 +55,22 @@ pub fn indexOfScalar(comptime T: type, comptime block_size: usize, slice: []cons
     const mask = @splat(block_size, value);
     var index: usize = 0;
 
-    // Vector scanning
-    while (index < slice.len - block_size) : (index += block_size) {
-        var subslice = slice[index .. index + block_size];
-        var vector: Vector = subslice[0..block_size].*;
+    if (slice.len > block_size) {
+        while (index < slice.len - block_size) : (index += block_size) {
+            var subslice = slice[index .. index + block_size];
+            var vector: Vector = subslice[0..block_size].*;
 
-        // Once a region is verified, subscan
-        if (@reduce(.Min, vector ^ mask) == 0) {
-            for (subslice) |subvalue, subindex| {
-                if (subvalue == value) return index + subindex;
+            // Once a region is verified, subscan
+            if (@reduce(.Min, vector ^ mask) == 0) {
+                for (subslice) |subvalue, subindex| {
+                    if (subvalue == value) return index + subindex;
+                }
+
+                // Impossible; the value must be in this region
+                unreachable;
             }
-
-            // Impossible; the value must be in this region
-            unreachable;
         }
-    }
+    } else index += block_size;
 
     // Subscan the remaining bytes
     for (slice[index - block_size ..]) |subvalue, subindex| {
@@ -68,15 +81,19 @@ pub fn indexOfScalar(comptime T: type, comptime block_size: usize, slice: []cons
 }
 
 test "indexOfScalar" {
+    var short_array = [_]u32{ 12, 18, 27, 78 };
+
+    try std.testing.expectEqual(@as(usize, 2), indexOfScalar(u32, 8, &short_array, 27).?);
+
     var array = [_]u32{ 12, 18, 27, 78, 39, 0, 67, 38, 30, 12, 87, 21, 3 };
 
-    try std.testing.expectEqual(@as(usize, 2), indexOfScalar(u32, 2, &array, 27).?);
-    try std.testing.expectEqual(@as(usize, 5), indexOfScalar(u32, 2, &array, 0).?);
+    try std.testing.expectEqual(@as(usize, 2), indexOfScalar(u32, 8, &array, 27).?);
+    try std.testing.expectEqual(@as(usize, 5), indexOfScalar(u32, 8, &array, 0).?);
     try std.testing.expectEqual(@as(usize, array.len - 1), indexOfScalar(u32, 2, &array, 3).?);
-    try std.testing.expectEqual(@as(usize, 8), indexOfScalar(u32, 2, &array, 30).?);
+    try std.testing.expectEqual(@as(usize, 8), indexOfScalar(u32, 8, &array, 30).?);
 
-    try std.testing.expect(indexOfScalar(u32, 2, &array, 82) == null);
-    try std.testing.expect(indexOfScalar(u32, 2, &array, 13) == null);
+    try std.testing.expect(indexOfScalar(u32, 8, &array, 82) == null);
+    try std.testing.expect(indexOfScalar(u32, 8, &array, 13) == null);
 }
 
 /// Parses an integer of a comptime-known length and radix
@@ -127,18 +144,21 @@ test "parseInt" {
     try std.testing.expectEqual(@as(usize, 69), parseInt(usize, 7, "1000101", 2));
 }
 
+/// Runs ~80x faster on my machine; larger blocks are better (use getPreferredBitSize!)
 pub fn min(comptime T: type, comptime block_size: usize, slice: []const T) T {
     const Vector = std.meta.Vector(block_size, T);
     var index: usize = 1;
     var best: T = slice[0];
 
-    while (index < slice.len - block_size) : (index += block_size) {
-        var vector: Vector = slice[index..][0..block_size].*;
-        var maybe_min = @reduce(.Min, vector);
+    if (slice.len > block_size) {
+        while (index < slice.len - block_size) : (index += block_size) {
+            var vector: Vector = slice[index..][0..block_size].*;
+            var maybe_min = @reduce(.Min, vector);
 
-        if (maybe_min < best)
-            best = maybe_min;
-    }
+            if (maybe_min < best)
+                best = maybe_min;
+        }
+    } else index += block_size;
 
     for (slice[index - block_size ..]) |subvalue| {
         if (subvalue < best)
@@ -149,22 +169,28 @@ pub fn min(comptime T: type, comptime block_size: usize, slice: []const T) T {
 }
 
 test "min" {
+    var small_array = [_]i32{ 12, 18, 27 };
+    try std.testing.expectEqual(@as(i32, 12), min(i32, 4, &small_array));
+
     var array = [_]i32{ 12, 18, 27, 78, 39, 0, 67, 38, 30, 12, 87, 21, 3, -89, 712, 90 };
     try std.testing.expectEqual(@as(i32, -89), min(i32, 4, &array));
 }
 
+/// Runs ~80x faster on my machine; larger blocks are better (use getPreferredBitSize!)
 pub fn max(comptime T: type, comptime block_size: usize, slice: []const T) T {
     const Vector = std.meta.Vector(block_size, T);
     var index: usize = 1;
     var best: T = slice[0];
 
-    while (index < slice.len - block_size) : (index += block_size) {
-        var vector: Vector = slice[index..][0..block_size].*;
-        var maybe_max = @reduce(.Max, vector);
+    if (slice.len > block_size) {
+        while (index < slice.len - block_size) : (index += block_size) {
+            var vector: Vector = slice[index..][0..block_size].*;
+            var maybe_max = @reduce(.Max, vector);
 
-        if (maybe_max > best)
-            best = maybe_max;
-    }
+            if (maybe_max > best)
+                best = maybe_max;
+        }
+    } else index += block_size;
 
     for (slice[index - block_size ..]) |subvalue| {
         if (subvalue > best)
@@ -175,6 +201,9 @@ pub fn max(comptime T: type, comptime block_size: usize, slice: []const T) T {
 }
 
 test "max" {
+    var small_array = [_]i32{ 12, 18, 27 };
+    try std.testing.expectEqual(@as(i32, 27), max(i32, 4, &small_array));
+
     var array = [_]i32{ 69, 87, 42, 420, 696969, 89, 45678987 };
     try std.testing.expectEqual(@as(i32, 45678987), max(i32, 4, &array));
 }
